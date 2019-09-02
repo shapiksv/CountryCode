ade053f2d6bf4f88a8bfa6daec65ff5b7e8b5f2c
shapiksv <51991973+shapiksv@user.noreply.github.com>
2019-09-02 22:22:50 +0300

    Initial commit

---
+++ src/CountryCode.java
@@ -0,0 +1,74 @@
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountryCode {
+    public static Map<String, String> countries = new HashMap<String, String>();
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter implements Contact, Customer{
+
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String phoneCode = "+" + data.getCountryPhoneCode();
+            String phoneNumber = new DecimalFormat("0000000000").format(data.getPhoneNumber());
+            return phoneCode + phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d{2})(\\d+)", "($1)$2-$3-$4");
+        }
+    }
+
+
+    public static interface IncomeData  {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example: 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67
+    }
+}
